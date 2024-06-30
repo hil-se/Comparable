6f46a24e3377e777574da8edee965022859fb926
Xiaoyin Xi <xixiaoyin1111@gmail.com>
2024-06-30 16:57:35 -0400

    vggface

--- Code/ImageExp/Classification.py
+++ Code/ImageExp/Classification.py
@@ -14,7 +14,7 @@ def learn(train_data,
           validation_data=None,
           y_true=[],
           patience=10,
-          batch_size=8,
+          batch_size=4,
           shared=False,
           height=250,
           width=250):
