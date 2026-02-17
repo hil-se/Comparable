f34e37cfb0440d99609e568ae70d8a096b08e48c
albertxi <xixiaoyin1111@gmail.com>
2026-02-17 14:33:51 -0500

    Update dataset in synthetic.py and add .python-version file; remove unused rank.py

--- Code/Synthetic/synthetic.py
+++ Code/Synthetic/synthetic.py
@@ -871,7 +871,7 @@ def run_experiments(
 
 
 if __name__ == "__main__":
-    DATASET = "german"  # scut, adult, german, heart, compas, comm, lsac
+    DATASET = "heart"  # scut, adult, german, heart, compas, comm, lsac
     NUM_RUNS = 10
     USE_ALL_PAIRS = True
     NUM_COMP_TRAIN = 1
