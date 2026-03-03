9ed341d9732747b08148543bf66b46a0978f917e
albertxi <xixiaoyin1111@gmail.com>
2026-03-03 18:53:12 +0800

    Update dataset and experiment parameters for improved training configuration

--- Code/Synthetic/synthetic.py
+++ Code/Synthetic/synthetic.py
@@ -1002,12 +1002,12 @@ def run_experiments(
 
 
 if __name__ == "__main__":
-    DATASET = "scut"  # scut, adult, german, heart, compas, comm, lsac
-    NUM_RUNS = 5
+    DATASET = "comm"  # scut, adult, german, heart, compas, comm, lsac
+    NUM_RUNS = 10
     USE_ALL_PAIRS = False  # Set False to use a fixed number of training pairs per instance.
-    NUM_COMP_TRAIN = 1
+    NUM_COMP_TRAIN = 10
     TRAIN_FAIRREG = False  # Set False to disable FairReg model training.
-    NUM_COMP_PAIRS_RATIO = 0.1
+    NUM_COMP_PAIRS_RATIO = 0.01
     MODEL_EPOCHS = 100
 
     run_experiments(
